6438035b212dbeb7d35d2b324b1b6001b0543602
omeralitosun <omerttosu@gmail.com>
2023-03-18 16:54:48 +0300

    Initial commit

---
+++ src/main/java/kodlama/io/rentacar/repository/concretes/InMemoryBrandRepository.java
@@ -0,0 +1,26 @@
+package kodlama.io.rentacar.repository.concretes;
+
+import kodlama.io.rentacar.entities.concretes.Brand;
+import kodlama.io.rentacar.repository.abstracts.BrandRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class InMemoryBrandRepository implements BrandRepository {
+    List<Brand> brands;
+
+    public InMemoryBrandRepository(){
+        brands = new ArrayList<Brand>();
+        brands.add(new Brand(1,"Mercedes"));
+        brands.add(new Brand(2,"BMW"));
+        brands.add(new Brand(3,"Audi"));
+    }
+
+    @Override
+    public List<Brand> getAll() {
+        return brands;
+    }
+
+}
